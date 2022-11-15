68021734d1ae949ef9889b02380d50cd16008c9f
kwakhyun <khyun2348@naver.com>
2022-11-15 23:59:34 +0900

    Feat: initial setting

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { RecoilRoot } from 'recoil';
+import { GlobalStyle, theme } from './styles';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
+  </React.StrictMode>
+);
