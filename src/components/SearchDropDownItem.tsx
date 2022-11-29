8a4b35db0ff8a74a7ee34e4df661b2dda016e199
kwakhyun <khyun2348@naver.com>
2022-11-29 21:07:50 +0900

    Feat: search components

---
+++ src/components/SearchDropDownItem.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/no-array-index-key */
+import styled from 'styled-components';
+import { BiSearch } from 'react-icons/bi';
+
+interface SearchDropDownItemProps {
+  sickNm: string;
+  searchValue: string;
+  className: string;
+}
+
+function SearchDropDownItem({ sickNm, searchValue, className }: SearchDropDownItemProps) {
+  const regex = new RegExp(`(${searchValue})`, 'gi');
+
+  return (
+    <StyledSearchDropDownItem className={className}>
+      <BiSearch />
+      {sickNm.split(regex).map((word, index) => (word === searchValue ? <strong key={index}>{word}</strong> : word))}
+    </StyledSearchDropDownItem>
+  );
+}
+
+export default SearchDropDownItem;
+
+const StyledSearchDropDownItem = styled.li`
+  font-size: 1rem;
+  display: flex;
+  align-items: center;
+  padding: 0.8rem;
+  overflow-wrap: break-word;
+  .selected {
+    background-color: lightgray;
+  }
+  > svg {
+    font-size: 1.2rem;
+    margin-right: 10px;
+    color: ${({ theme }) => theme.color.gray};
+  }
+  > strong {
+    font-weight: 700;
+  }
+`;
